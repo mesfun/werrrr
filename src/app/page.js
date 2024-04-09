249097bebe47f239a54b0f7b372eea9f930ae1a8
mesfun <mesfun@hotmail.com>
2024-04-09 10:09:31 -0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
 
   useEffect(() => {
     setLoading(true);
-  const url = 'https://api.jsonsilo.com/public/d1166487-7a88-4c53-9be7-94d20dbd04d4/users';
+  const url = 'https://api.jsonsilo.com/public/d1166487-7a88-4c53-9be7-94d20dbd04d4';
     const headers = {
         'Content-Type': 'application/json'
     };
@@ -60,7 +60,7 @@ const Filter = (event) => {
             </tr>
           </thead>
           <tbody>
-            {users.map(user => (
+            {data.map(user => (
               <tr key={user.id}>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
